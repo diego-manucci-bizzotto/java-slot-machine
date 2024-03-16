5aac51e2939248e5ee151855920f25f688ea226a
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-03-16 10:04:35 -0300

    Refactor: Substituir Thymeleaf por jsp

--- src/main/java/br/dbizzotto/servlets/HelloWorldServlet.java
+++ src/main/java/br/dbizzotto/servlets/HelloWorldServlet.java
@@ -1,45 +1,19 @@
 package br.dbizzotto.servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.web.servlet.IServletWebExchange;
-import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
 
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-
-    TemplateEngine templateEngine;
-
     @Override
-    public void init() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setSuffix(".html");
-        templateResolver.setPrefix("templates/");
-        templateResolver.setCacheable(false);
-
-        templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
-        response.setContentType("text/html");
-
-        JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(request.getServletContext());
-        IServletWebExchange exchange = application.buildExchange(request, response);
-        WebContext context = new WebContext(exchange);
-
-        String renderedHtml = templateEngine.process("hello", context);
-        response.getWriter().write(renderedHtml);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/hello.jsp");
+        dispatcher.forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html xmlns:th="http://www.thymeleaf.org" lang="pt-br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <title>Hello World</title>
 </head>
 <body>
-<h1>Hello, World!</h1>
+    <h1>Hello, World!</h1>
+    <p>This is a JSP page served by Tomcat.</p>
 </body>
 </html>
